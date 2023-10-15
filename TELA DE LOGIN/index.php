0556213de0c75d9a16b1a1432d9304fe60320bd3
Gerson Santos <gersones@outlook.com>
2023-10-15 16:31:21 -0300

    a
    aa

---
+++ TELA DE LOGIN/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href ="CSS/estilo.css">
+    <title>Projeto Login</title>
+</head>
+<body>
+<div id="corpo-form">
+    <h1>ENTRAR</h1>
+    <form method="POST" action="processa.php">
+        <input type="email" placeholder="Usuário">
+        <input type="password" placeholder="senha">
+        <input type="submit" placeholder="Acessar">
+    </form>
+</div>
+</body>
+</html>
